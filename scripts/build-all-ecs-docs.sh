3e41f4aaf5f16db93289c32e260b6e9b8c266be8
Felix Erkinger <felix@erkinger.at>
2016-12-10 22:39:43 +0100

    update scripts

--- scripts/build-all-ecs-docs.sh
+++ scripts/build-all-ecs-docs.sh
@@ -20,3 +20,8 @@ cd $srcpath/homepage
 rm -rf blog/*
 tinker -b
 mv blog/html/ $srcpath/_build/homepage
+
+cd $srcpath/_build
+> .nojekyll
+printf "%b" "FROM nginx\nCOPY . /usr/share/nginx/html\n" > Dockerfile
+printf "%b" '<!doctype html>\n<html>\n<head>\n<meta http-equiv="REFRESH" content="0; url=./homepage/index.html" />\n<title></title>\n</head>\n<body></body>\n</html>\n' > index.html
--- scripts/force-push-build.sh
+++ scripts/force-push-build.sh
@@ -9,9 +9,6 @@ fi
 ECS_HOMEPAGE_GIT_SOURCE="${ECS_HOMEPAGE_GIT_SOURCE:-ssh://git@gogs.omoikane.ep3.at:10022/ecs/ecs-homepage.git}"
 
 cd  $srcpath/_build
-> .nojekyll
-printf "%b" "FROM nginx\nCOPY . /usr/share/nginx/html\n" > Dockerfile
-printf "%b" "<!doctype html>\n<html>\n<head>\n<meta http-equiv="REFRESH" content="0; url=./homepage/index.html" />\n<title></title>\n</head>\n<body></body>\n</html>\n" > index.html
 git init
 git add --all .
 git commit -a -m "Update doc"
