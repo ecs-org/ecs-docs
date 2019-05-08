efe22dcb9538c266bbdee465fdb114bb4e673270
Felix Erkinger <felix@erkinger.at>
2019-05-08 20:00:45 +0200

    update build script and os dependencies

--- scripts/build-all-ecs-docs.sh
+++ scripts/build-all-ecs-docs.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -eo pipefail
 
 srcpath="$(readlink -e $(dirname $(readlink -e "$0"))/..)"
-cd $srcpath
+cd "$srcpath"
 rm -rf _build
 mkdir -p _build
 . _env/bin/activate
 
 for d in user-manual-de admin-manual development; do
-    make -C $d clean html latexpdf
-    if test $? -ne 0; then
+    make -C $d clean html latexpdf && err=$? || err=$?
+    if test $err -ne 0; then
         echo "Error: building of $d failed"
         exit 1
     fi
@@ -16,15 +17,24 @@ for d in user-manual-de admin-manual development; do
     mv $d/_build/latex/ecs-$d.pdf _build/
 done
 
-cd $srcpath/homepage
-make clean html
-mv _build/ $srcpath/_build/homepage
+cd "$srcpath/homepage"
+make clean html && err=$? || err=$?
+if test $err -ne 0; then
+    echo "Error: building of homepage failed"
+    exit 1
+fi
+mv _build/ "$srcpath/_build/homepage"
 
-cd $srcpath/_build
-> .nojekyll
+cd "$srcpath/_build"
+true > .nojekyll
 printf "%b" "FROM nginx\nCOPY . /usr/share/nginx/html\n" > Dockerfile
 printf "%b" '<!doctype html>\n<html>\n<head>\n<meta http-equiv="REFRESH" content="0; url=./homepage/index.html" />\n<title></title>\n</head>\n<body></body>\n</html>\n' > index.html
-if test -f ~/work/egiz/pdf-as-web-4.1.0.war; then
-    echo "Warning: including old pdf-as-web-4.1.0.war in _build"
-    cp ~/work/egiz/pdf-as-web-4.1.0.war .
-fi
+
+for i in pdf-as-web-4.1.0.war defaultConfig-4.1.1.zip; do
+    if test ! -e "$srcpath/../egiz/$i"; then
+        echo "Error: $i not found in ../egiz, abort!"
+        exit 1
+    fi
+    echo "Warning: including old $i in _build"
+    cp "$srcpath/../egiz/$i" .
+done
--- scripts/os-deps-ecs-docs.sh
+++ scripts/os-deps-ecs-docs.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-plantuml_sha256="e500ea94600ecade4af7262b32ea20a1c884de0013f4a3f07aeae02d594424ab"
+plantuml_url="https://netix.dl.sourceforge.net/project/plantuml/1.2017.15/plantuml.1.2017.15.jar"
+plantuml_sha256="c0bf0f8fcfa68de6cb12e8c5c67ec61efffdbfb7ea32974cb8be4a38df94a415"
 need_download=true
-if test -e "/opt/plantuml.jar"; then
-    echo "$plantuml_sha256 */opt/plantuml.jar" | sha256sum -c -
+if test -e "/usr/local/lib/plantuml.jar"; then
+    echo "$plantuml_sha256 */usr/local/lib/plantuml.jar" | sha256sum -c -
     if test $? -eq 0; then need_download=false; fi
 fi
 if $need_download; then
-    curl -f -m 120 --connect-timeout 10 -s -S -o /tmp/plantuml.jar http://netcologne.dl.sourceforge.net/project/plantuml/plantuml.8048.jar
+    curl -f -m 120 --connect-timeout 10 -s -S -o /tmp/plantuml.jar "$plantuml_url"
     echo "$plantuml_sha256 */tmp/plantuml.jar" | sha256sum -c -
     if test $? -ne 0; then
         echo "error: plantuml checksum missmatch"
         exit 1
     fi
-    cp -f /tmp/plantuml.jar  /opt/plantuml.jar
+    cp -f /tmp/plantuml.jar  /usr/local/lib/plantuml.jar
+    cat > /usr/local/bin/plantuml <<"EOF"
+#!/bin/bash
+java -jar /usr/local/lib/plantuml.jar $@
+EOF
+    chmod +x /usr/local/bin/plantuml
 fi
 
-DEBIAN_FRONTEND=noninteractive apt-get update -y && apt-get install -y dvipng libjs-mathjax texlive-fonts-recommended texlive-latex-extra texlive-latex-recommended texlive-lang-english texlive-lang-german default-jre curl graphviz python3 python3-dev python3-setuptools cpp libxml2-dev libxslt-dev zlib1g-dev
+DEBIAN_FRONTEND=noninteractive apt-get update -y && apt-get install -y dvipng libjs-mathjax latexmk texlive-fonts-recommended texlive-latex-extra texlive-latex-recommended texlive-lang-english texlive-lang-german default-jre curl graphviz python3 python3-dev python3-setuptools cpp libxml2-dev libxslt-dev zlib1g-dev
 
-printf '%b' '#!/bin/bash\njava -jar /opt/plantuml.jar $@\n' > /usr/local/bin/plantuml
-chmod +x /usr/local/bin/plantuml
-printf '%b' '#!/bin/bash\ncpp | dot $@\n' > /usr/local/bin/cpp-dot
+cat > /usr/local/bin/cpp-dot <<"EOF"
+#!/bin/bash
+cpp | dot $@
+EOF
 chmod +x /usr/local/bin/cpp-dot
 
 if ! which pip3; then easy_install3 pip; fi
