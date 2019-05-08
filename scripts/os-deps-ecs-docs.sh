9e5f3b053fc852f939e5bf7e9be3894a892bda53
Felix Erkinger <felix@erkinger.at>
2019-05-08 20:02:52 +0200

    add ghp-import to os-deps

--- scripts/os-deps-ecs-docs.sh
+++ scripts/os-deps-ecs-docs.sh
@@ -22,7 +22,7 @@ EOF
     chmod +x /usr/local/bin/plantuml
 fi
 
-DEBIAN_FRONTEND=noninteractive apt-get update -y && apt-get install -y dvipng libjs-mathjax latexmk texlive-fonts-recommended texlive-latex-extra texlive-latex-recommended texlive-lang-english texlive-lang-german default-jre curl graphviz python3 python3-dev python3-setuptools cpp libxml2-dev libxslt-dev zlib1g-dev
+DEBIAN_FRONTEND=noninteractive apt-get update -y && apt-get install -y dvipng libjs-mathjax latexmk texlive-fonts-recommended texlive-latex-extra texlive-latex-recommended texlive-lang-english texlive-lang-german default-jre curl graphviz python3 python3-dev python3-setuptools cpp libxml2-dev libxslt-dev zlib1g-dev ghp-import
 
 cat > /usr/local/bin/cpp-dot <<"EOF"
 #!/bin/bash
