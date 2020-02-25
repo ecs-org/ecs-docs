c0c2d3083d4499fd47860982fd9fd0171aaad222
Felix Erkinger <felix@erkinger.at>
2020-02-25 17:26:13 +0100

    regenerate docs

--- development/_static/documentation_options.js
+++ development/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020-02-25@17:25',
+    VERSION: '2020-02-25@17:26',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
