48e675cd82ed4742750159228ed6ccbb1e7bb438
Felix Erkinger <felix@erkinger.at>
2020-05-31 16:09:05 +0200

    add Vinzenz Gruppe to Copyright, reformat using black

--- homepage/pelicanconf.py
+++ homepage/pelicanconf.py
@@ -4,54 +4,60 @@ from __future__ import unicode_literals
 import os
 from datetime import datetime
 
-AUTHOR = datetime.now().strftime("%Y") +' Medizinische Universität Wien, Medizinische Universität Innsbruck, Medizinische Universität Graz, Johannes Kepler Universität Linz, Karl Landsteiner Privatuniversität für Gesundheitswissenschaften, Land Salzburg'
-SITENAME = 'ECS - Ethics Committee System'
-SITEURL = ''
-#THEME = 'simple'
-THEME = '_themes/pelican-bootstrap3'
+AUTHOR = (
+    datetime.now().strftime("%Y")
+    + " Medizinische Universität Wien, Medizinische Universität Innsbruck, Medizinische Universität Graz, Johannes Kepler Universität Linz, Karl Landsteiner Privatuniversität für Gesundheitswissenschaften, Land Salzburg, Vinzenz Gruppe"
+)
+
+SITENAME = "ECS - Ethics Committee System"
+SITEURL = ""
+# THEME = 'simple'
+THEME = "_themes/pelican-bootstrap3"
 PLUGIN_PATHS = [
-    os.path.abspath(os.path.join(os.path.dirname(__file__), '_plugins')),
-  ]
-PLUGINS = ['i18n_subsites',]
-JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "_plugins")),
+]
+PLUGINS = [
+    "i18n_subsites",
+]
+JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
 
 OUTPUT_SOURCES = False
-OUTPUT_PATH = '_build'
-PATH = '.'
-PAGE_PATHS = ['pages']
-ARTICLE_PATHS = ['articles']
-STATIC_PATHS = ['static']
+OUTPUT_PATH = "_build"
+PATH = "."
+PAGE_PATHS = ["pages"]
+ARTICLE_PATHS = ["articles"]
+STATIC_PATHS = ["static"]
 
-DEFAULT_LANG = 'en'
-DEFAULT_DATE = 'fs'
+DEFAULT_LANG = "en"
+DEFAULT_DATE = "fs"
 DEFAULT_PAGINATION = False
-DEFAULT_DATE_FORMAT = '%a %d %B %Y'
-TIMEZONE = 'Europe/Paris'
+DEFAULT_DATE_FORMAT = "%a %d %B %Y"
+TIMEZONE = "Europe/Paris"
 TYPOGRIFY = False
 
-PAGE_ORDER_BY = 'order' # use meta tag order to define pages order
+PAGE_ORDER_BY = "order"  # use meta tag order to define pages order
 
 # Menu
-DISPLAY_PAGES_ON_MENU = True # do not display, because we want to override all
+DISPLAY_PAGES_ON_MENU = True  # do not display, because we want to override all
 DISPLAY_CATEGORIES_ON_MENU = False
 # A list of tuples (Title, URL) for additional menu items to appear at the beginning of the main menu.
 MENUITEMS = (
-#    ( 'Custom', '#'),
+    #    ( 'Custom', '#'),
 )
 
 # Links
 # A list of tuples (Title, URL) for links to appear on the header.
 LINKS = (
-#    ( 'Test Header Link', '#'),
+    #    ( 'Test Header Link', '#'),
 )
 # A list of tuples (Title, URL) to appear in the “social” section.
 SOCIAL = (
-#    ('Test Social Link', '#'),
+    #    ('Test Social Link', '#'),
 )
 
 # Tags are not needed
-TAGS_SAVE_AS = ''
-TAG_SAVE_AS = ''
+TAGS_SAVE_AS = ""
+TAG_SAVE_AS = ""
 
 # Feed generation is not needed
 FEED_ALL_ATOM = None
@@ -66,9 +72,9 @@ SHOW_ARTICLE_CATEGORY = False
 SHOW_DATE_MODIFIED = False
 
 # Add custom CSS
-CUSTOM_CSS = 'static/custom.css'
+CUSTOM_CSS = "static/custom.css"
 
 # Theme options
 HIDE_SIDEBAR = True
 BOOTSTRAP_NAVBAR_INVERSE = True
-SITELOGO = 'static/favicon.ico'
+SITELOGO = "static/favicon.ico"
