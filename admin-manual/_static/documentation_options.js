4d3ea1a9ac5b14509e6393ab4383310c4265c07e
Felix Erkinger <felix@erkinger.at>
2020-05-31 15:23:10 +0200

    regenerate docs

--- admin-manual/_static/documentation_options.js
+++ admin-manual/_static/documentation_options.js
@@ -1,10 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020-04-14@14:49',
+    VERSION: '2020-05-31@15:22',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
